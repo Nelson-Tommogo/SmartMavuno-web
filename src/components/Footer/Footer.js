7b188076a3ab241bf504ce69c316c111520b0055
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-05-17 12:22:28 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,72 @@ import './Footer.css'; // Import Footer.css for styling
 
 function Footer() {
   return (
-    <footer className="footer-container">
-      <p>&copy; 2024 Smart Mavuno. All rights reserved.</p>
-    </footer>
+    <div className="footer-container">
+      <div className="newsletter-section">
+        <img src="/smart.webp" alt="Newsletter" className="newsletter-image" />
+        <div className="vertical-line"></div>
+        <div className="newsletter-content">
+          <p>Subscribe to our newsletter to get our updates and recommendations</p>
+          <div className="input-icon-wrapper">
+            <i className="fas fa-envelope email-icon"></i>
+            <input type="email" placeholder="Enter your email" className="newsletter-input" />
+            
+            <button className="subscribe-button">Subscribe</button>
+          </div>
+          
+        </div>
+      </div>
+      
+
+      <div className="footer-main">
+        <div className="footer-section">
+          <h4>Download our app Today.</h4>
+          <img src="path/to/google-play-badge.png" alt="Download on Google Play" className="app-download"/>
+        </div>
+
+        <div className="footer-section">
+          <h4>Company</h4>
+          <ul>
+            <li>About us</li>
+            <li>Community</li>
+            <li>Testimonies</li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <h4>Support</h4>
+          <ul>
+            <li>Help center</li>
+            <li>Tweet @ us</li>
+            <li>Send a mail</li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <h4>Links</h4>
+          <ul>
+            <li>Download App</li>
+            <li>Shop now</li>
+            <li>Donation Busket</li>
+          </ul>
+        </div>
+
+        <div className="footer-section">
+          <h4>Follow our socials</h4>
+          <ul className="social-links">
+            <li><i className="fab fa-xing"></i> smartmavuno</li>
+            <li><i className="fab fa-instagram"></i> smartmavuno</li>
+            <li><i className="fab fa-linkedin"></i> smartmavuno</li>
+            <li><i className="fab fa-facebook"></i> smartmavuno</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>Copyright &copy;2023 SmartMavuno. All Rights Reserved</p>
+      </div>
+    </div>
   );
 }
 
 export default Footer;
-
