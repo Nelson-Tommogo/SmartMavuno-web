ac409006a66acf31040b5ab98ef219cbe8c4b1fa
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-06-14 11:33:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-// src/App.js
-
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp/SignUp';
-import './App.css';
+import Dashboard from './components/MainContent/Dashboard/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome for icons
 
 function App() {
   return (
-    <div className="App">
-      <SignUp />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
