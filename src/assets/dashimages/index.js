599b99bd2f954b7c0dc5c9518f578e5434530be6
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-06-14 11:37:53 +0300

    Add files via upload

---
+++ src/assets/dashimages/index.js
@@ -0,0 +1,19 @@
+// index.js inside dashimages folder
+import main from './main.png';
+import nav from './nav.png';
+import maize from './maize.png';
+import banana from './banana.png';
+import hoho from './hoho.png';
+import strawberry from './strawberry.png';
+
+const images = {
+  main,
+  nav,
+  maize,
+  banana,
+  hoho, // Corrected comma to colon here
+  strawberry
+};
+
+export default images;
+
