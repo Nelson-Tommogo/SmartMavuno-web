468eff5aca55901a9124119c882d03317256b063
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-06-18 23:56:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp/SignUp';
 import Dashboard from './components/MainContent/Dashboard/Dashboard';
+import Wallet from './components/MainContent/Wallet/Wallet'; // Import Wallet component
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome for icons
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/wallet" element={<Wallet />} /> {/* Add Wallet route */}
       </Routes>
     </Router>
   );
