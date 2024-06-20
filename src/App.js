a7d3fcbf27d63daaff9977bbff7b0243f0969f9a
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-06-20 21:35:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp/SignUp';
 import Dashboard from './components/MainContent/Dashboard/Dashboard';
-import Wallet from './components/MainContent/Wallet/Wallet'; // Import Wallet component
+import Wallet from './components/MainContent/Wallet/Wallet';
+import Donation from './components/MainContent/Donations/Donation'; // Import the Donation component
+import ThankYou from './components/MainContent/ThankYou/ThankYou';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome for icons
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/wallet" element={<Wallet />} /> {/* Add Wallet route */}
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/donations" element={<Donation />} /> {/* Add the route for Donation */}
+        <Route path="/thank-you" element={<ThankYou />} /> 
       </Routes>
     </Router>
   );
