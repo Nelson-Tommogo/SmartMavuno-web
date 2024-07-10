c92e7b32fb4378d20fbebbfdac02da7d6203842d
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-07-10 15:43:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from './components/MainContent/Dashboard/Dashboard';
 import Wallet from './components/MainContent/Wallet/Wallet';
 import Donation from './components/MainContent/Donations/Donation';
 import ThankYou from './components/MainContent/ThankYou/ThankYou';
+import ReachUs from './components/MainContent/ReachUs/ReachUs'; // Import ReachUs component
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -19,10 +20,10 @@ function App() {
         <Route path="/wallet" element={<Wallet />} />
         <Route path="/donations" element={<Donation />} />
         <Route path="/thank-you" element={<ThankYou />} />
+        <Route path="/reach-us" element={<ReachUs />} /> {/* Add the ReachUs route */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
