c361a86cbac5877065e402f657d4507605ebf916
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-07-10 15:53:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
-import SignUp from './components/SignUp/SignUp'; // Import SignUp component
+import SignUp from './components/SignUp/SignUp';
 import Dashboard from './components/MainContent/Dashboard/Dashboard';
 import Wallet from './components/MainContent/Wallet/Wallet';
 import Donation from './components/MainContent/Donations/Donation';
 import ThankYou from './components/MainContent/ThankYou/ThankYou';
-import ReachUs from './components/MainContent/ReachUs/ReachUs'; // Import ReachUs component
+import ReachUs from './components/MainContent/ReachUs/ReachUs';
+import Login from './components/Login/Login'; // Import Login component
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -16,11 +17,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} /> {/* Add the Login route */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/wallet" element={<Wallet />} />
         <Route path="/donations" element={<Donation />} />
         <Route path="/thank-you" element={<ThankYou />} />
-        <Route path="/reach-us" element={<ReachUs />} /> {/* Add the ReachUs route */}
+        <Route path="/reach-us" element={<ReachUs />} />
       </Routes>
     </Router>
   );
